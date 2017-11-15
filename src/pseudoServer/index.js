99d5d3425b5d45365d54c3c3c5ce77aa4a0f7a97
dominikglandorf <do.gl@gmx.de>
2017-11-15 09:08:08 +0100

    beautify form

--- src/pseudoServer/index.js
+++ src/pseudoServer/index.js
@@ -72,5 +72,5 @@ app.post('/content', a(async (req, res) => {
 }));
 
 app.listen(8080, () => {
-  console.log('server listening on port 8080')
+  console.log('Pseudo server listening on port 8080')
 });
